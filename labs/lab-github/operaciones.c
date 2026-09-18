f9ba42ea7468d38379cb1534da8941caeaa932b6
melodywT <cecondori@frba.utn.edu.ar>
2026-09-18 15:30:10 -0300

    Agrega comentario explicativo en multiplicar

--- labs/lab-github/operaciones.c
+++ labs/lab-github/operaciones.c
@@ -12,6 +12,7 @@ int restar(int a, int b) {
     return a - b;
 }
 
+/* Multiplica dos enteros usando el operador * */
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 int multiplicar(int a, int b) {
     return a * b;
